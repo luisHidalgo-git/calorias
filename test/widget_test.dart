fe791d9abaf2d60f87d115b0481b97a6937f7509
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2025-06-23 13:19:28 -0600

    primer intento de ejecucion

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:smartwatch/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Smartwatch calorie counter test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(SmartwatchCalorieApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('PASOS'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
+    // Tap the '+' button and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('10'), findsOneWidget);
   });
 }
